5d39f5502a8e6763c2c50712a20672b5aace244a
thibautvalour <74783392+thibautvalour@users.noreply.github.com>
2024-02-28 15:39:06 +0100

    ...

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Trading
+# Investissement
+
+This investment strategy is a Dollard-Cost Averaging plus an initial investment.
+Reference ETF: "BNP Paribas Easy S&P 500 UCITS ETF EUR Capitalisation" (FR0011550185)
+
+The return takes into account dividends (100% immediatly reinvested), management fees (0.153%/year),entry (3%) and exit (3%) fees,
+and taxex (PEA after 5 years: 17,2% on the gain)
+
+Inflation can also be used to see the ammount of € invested with adjusted value at the end of the period.
+
+Furthemore, the strategy can be compared to a simple saving account such as Livret A (No Limit)
+
+Comparison with Gold is also available.
+
+List of possible risk factors:
+
+- Market risk (S&P 500 is not performing well)
+- Currency risk (EUR/USD)
+- Inflation risk (not really a risk)
+- BNP Paribas failure
+
+- Tracking error
+- Liquidity risk
---
+++ data_loading_funcs.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import datetime
+
+def get_monthly_stock_with_dividends(stock, start, end, yearly_fee_rate=0.00153):
+
+        if start < datetime.datetime(1993, 3, 19):
+            raise ValueError('Start date must be later than 1993-03-19')
+
+        # Download historical prices and dividends
+        stock_data = yf.download(stock, start=start, end=end)
+
+        
+
+        dividends = yf.Ticker(stock).dividends
+        try: dividends.index = dividends.index.tz_localize(None)
+        except TypeError:  pass
+
+        dividends = dividends[(dividends.index >= start) & (dividends.index <= end)]
+
+        # Adjust the stock prices with reinvested dividends
+        adjusted_close = stock_data['Close'].copy()
+        for date, dividend in dividends.items():
+            adjustment_factor = 1 + dividend / adjusted_close.loc[:date].iloc[-2]
+            adjusted_close.loc[date:] *= adjustment_factor
+        stock_data['Adjusted Close'] = adjusted_close
+
+        monthly_prices = stock_data.resample('M').first()
+
+        # Adjust the monthly prices to include the yearly fee
+        monthly_fee = (1+yearly_fee_rate)**(1/12) - 1
+        monthly_prices['Adjusted Close return'] = monthly_prices['Adjusted Close'].pct_change().fillna(0) - monthly_fee
+        monthly_prices['cumulative_return'] = (1 + monthly_prices['Adjusted Close return']).cumprod()
+        monthly_prices['Adjusted Close'] = monthly_prices['Adjusted Close'].iloc[0] * monthly_prices['cumulative_return']
+
+        return monthly_prices
